a32fe1d167204f963cde82227aa661c285bbb723
Trần Hưng <Trn Hng>
2016-06-04 20:19:00 +0700

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,7 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['uploads/([a-z 0-9 - / . - _]+)']= "welcome/dataupload/file/$1";
+$route['uploads/([\s\S]+)']= "welcome/dataupload/file/$1";
 
 $route['home'] = 'welcome/home/';
 $route['drag'] = 'welcome/drag/';
