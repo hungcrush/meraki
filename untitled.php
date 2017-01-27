b04a13007925ef2d66404b7d29569b8f04e231ad
Trần Hưng <Trn Hng>
2017-01-28 00:16:15 +0700

    Update

--- untitled.php
+++ untitled.php
@@ -2,5 +2,5 @@
 $str = "SELECT DISTINCT t.*,n.title AS forum_title,u.user_group_id,u.display_style_group_id FROM xf_post p LEFT JOIN xf_thread t using (thread_id) LEFT JOIN xf_node n using(node_id) LEFT JOIN xf_user u ON t.last_post_user_id=u.user_id WHERE p.user_id='1' AND t.reply_count>0 AND `discussion_state`='visible' AND `message_state`='visible' ORDER BY p.post_date DESC LIMIT 15";
 
 $s = preg_match("/xf_userdf/", $str);
-
+echo 'OK';
 var_dump($s);
